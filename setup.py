80d27b5e78a5d4ac015be694d931823d3e85e3da
Aayush9029 <aayushpokharel36@gmail.com>
2022-01-19 16:40:46 -0500

    updated few files

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = []
 
 setup(
     name="ddna",
-    version="0.1.0",
+    version="0.1.1",
     author="Aayush Pokharel",
     author_email="aayushpokharel36@gmail.com",
     description="Encode and decode strings to DNA",
